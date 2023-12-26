ce49f70712b20f8a66e84825c7e269094fc41384
Xuyj <1603461929@qq.com>
2023-12-26 19:58:45 +0800

    eureka 配置

--- springcloud-eureka-parent/eureka-client02-consumer/src/main/java/com/itxuyj/controller/OrderController.java
+++ springcloud-eureka-parent/eureka-client02-consumer/src/main/java/com/itxuyj/controller/OrderController.java
@@ -1,10 +1,12 @@
 package com.itxuyj.controller;
 
 import com.itxuyj.domain.Goods;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 服务调用方
@@ -13,11 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/goods/{id}")
     public Goods findGoodsById(@PathVariable("id") int id) {
 
         System.out.println("findGoodsById..." + id);
-        //远程调用Goods服务中的findOne接口
-        return null;
+        /*
+            远程调用Goods服务中的findOne接口
+            使用RestTemplate
+            1.定义Bean  restTemplate
+            2.注入Bean
+            3.调用方法
+         */
+        return  restTemplate.getForObject("http://localhost:8098/goods/findOne/"+id, Goods.class);
     }
 }
