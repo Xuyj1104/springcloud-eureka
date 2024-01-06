57c79ca28368e0b895b934ee223401af19ea81d3
Xuyj <1603461929@qq.com>
2024-01-06 17:46:36 +0800

    Eureka 动态获取 ip  端口 实现接口调用

--- springcloud-eureka-parent/eureka-client02-consumer/src/main/java/com/itxuyj/ConsumerApplication.java
+++ springcloud-eureka-parent/eureka-client02-consumer/src/main/java/com/itxuyj/ConsumerApplication.java
@@ -2,6 +2,7 @@ package com.itxuyj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient   //该注解 在新版本中可以省略
+@EnableDiscoveryClient //激活DiscoveryClient  该注解 在新版本中也可以省略
 public class ConsumerApplication {
     public static void main(String[] args) {
 
--- springcloud-eureka-parent/eureka-client02-consumer/src/main/java/com/itxuyj/controller/OrderController.java
+++ springcloud-eureka-parent/eureka-client02-consumer/src/main/java/com/itxuyj/controller/OrderController.java
@@ -2,11 +2,15 @@ package com.itxuyj.controller;
 
 import com.itxuyj.domain.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * 服务调用方
@@ -17,6 +21,11 @@ public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
 
     @GetMapping("/goods/{id}")
     public Goods findGoodsById(@PathVariable("id") int id) {
@@ -29,6 +38,33 @@ public class OrderController {
             2.注入Bean
             3.调用方法
          */
-        return restTemplate.getForObject("http://localhost:8667/goods/findOne/" + id, Goods.class);
+
+        /*
+            动态从Eureka Server 中获取 provider 的 ip 和端口
+            1.注入DiscoveryClient 对象
+            2.调用方法
+         */
+
+        //演示discoveryClient 使用
+        List<ServiceInstance> instances = discoveryClient.getInstances("PROVIDER-SERVER");
+
+        //判断集合是否有数据
+        if (instances == null || instances.size() == 0) {
+            //集合没有数据
+            return null;
+        }
+
+        ServiceInstance serviceInstance = instances.get(0);
+        String host = serviceInstance.getHost(); //获取ip
+        int port = serviceInstance.getPort(); //获取端口
+
+        System.out.println(host);
+        System.out.println(port);
+
+        String url = "http://"+host+"+:"+port+"/goods/findOne/" + id;
+        //3.调用方法
+        Goods goods = restTemplate.getForObject("http://localhost:8667/goods/findOne/" + id, Goods.class);
+
+        return goods;
     }
 }
